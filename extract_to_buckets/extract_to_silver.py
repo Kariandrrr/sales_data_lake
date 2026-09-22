f8fce4c9df46821a005c4bba206f1ac4bafaf610
Suslova Karina <educationkarina08@gmail.com>
2026-09-22 09:52:15 +0300

    start to write extraction to silver layout 2

--- extract_to_buckets/extract_to_silver.py
+++ extract_to_buckets/extract_to_silver.py
@@ -2,11 +2,8 @@ import hashlib
 import io
 
 import pandas as pd
-from dotenv import load_dotenv
 
 from utils.create_s3_client import s3_client, BUCKET_NAME
-
-load_dotenv()
 
 
 def read_parquet(key: str) -> pd.DataFrame:
@@ -39,4 +36,26 @@ for col in timestamp_cols:
 df_orders = df_orders.dropna(subset=["order_purchase_timestamp"])
 write_parquet("silver/orders/orders.parquet", df_orders)
 
-print("2. Deduplicating customers...")
+print("2. Deduplicating  and masking PII...")
+df_customers = read_parquet("bronze/customers/customers.parquet")
+
+df_customers = df_customers.drop_duplicates(subset="customer_id")
+
+df_customers["customer_zip_code_prefix"] = df_customers[
+    "customer_zip_code_prefix"
+].apply(hash_pii)
+df_customers["customer_city"] = df_customers["customer_city"].str.title().str.strip()
+
+write_parquet("silver/customers/customers.parquet", df_customers)
+
+print("3. Deduplicating order items...")
+df_items = read_parquet("bronze/order_items/order_items.parquet")
+df_items = df_items.drop_duplicates(subset=["order_id", "order_item_id"])
+df_items = df_items[(df_items["price"] >= 0) & (df_items["freight_value"] <= 0)]
+
+write_parquet("silver/order_items/order_items.parquet", df_items)
+
+print("4. Deduplicating order payments...")
+df_payments = read_parquet("bronze/order_payments/order_payments.parquet")
+df_payments = df_payments.drop_duplicates(subset=["order_id", "payment_sequential"])
+write_parquet("silver/order_payments/order_payments.parquet", df_payments)
