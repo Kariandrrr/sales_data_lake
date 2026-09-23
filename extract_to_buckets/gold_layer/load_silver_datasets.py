83a28e9cb509aa97586fa6c63daef07a7f8a2b9c
Suslova Karina <educationkarina08@gmail.com>
2026-09-23 18:39:45 +0300

    load file from silver layout

---
+++ extract_to_buckets/gold_layer/load_silver_datasets.py
@@ -0,0 +1,29 @@
+import io
+import pandas as pd
+from utils.parquet_functions import read_parquet, write_parquet
+
+print("1. Loading silver datasets...")
+
+df_orders = read_parquet("silver/orders/orders.parquet")
+df_items = read_parquet("silver/order_items/order_items.parquet")
+df_customers = read_parquet("silver/customers/customers.parquet")
+df_products = read_parquet("silver/products/products.parquet")
+df_trans = read_parquet(
+    "silver/product_category_name_translation/product_category_name_translation.parquet"
+)
+
+df_products_enriched = df_products.merge(
+    df_trans, on="product_category_name", how="left"
+)
+
+df_products_enriched["product_category_name_english"] = df_products_enriched[
+    "product_category_name_english"
+].fillna("other")
+
+df_sales_base = (
+    df_items.merge(df_orders, on="order_id", how="inner")
+    .merge(df_customers, on="customer_id", how="left")
+    .merge(df_products_enriched, on="product_id", how="left")
+)
+
+df_sales_base["total_value"] = df_sales_base["price"] + df_sales_base["freigth_value"]
