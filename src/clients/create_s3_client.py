3056f6e9a25d2b9d31057a9ebeabe23d242e145d
Suslova Karina <educationkarina08@gmail.com>
2026-09-25 23:37:22 +0300

    delete unnecessary code

--- src/clients/create_s3_client.py
+++ src/clients/create_s3_client.py
@@ -1,18 +1,10 @@
-import os
-
 import boto3
-from dotenv import load_dotenv
 
-load_dotenv()
-
-MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT")
-MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
-MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
-BUCKET_NAME = "lakehouse"
+from ..config import settings
 
 s3_client = boto3.client(
     "s3",
-    endpoint_url=MINIO_ENDPOINT,
-    aws_access_key_id=MINIO_ACCESS_KEY,
-    aws_secret_access_key=MINIO_SECRET_KEY,
+    endpoint_url=settings.MINIO_ENDPOINT,
+    aws_access_key_id=settings.MINIO_ACCESS_KEY,
+    aws_secret_access_key=settings.MINIO_SECRET_KEY,
 )
