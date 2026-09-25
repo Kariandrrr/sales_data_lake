820bbe404bb887e422114019428e2685dff61f2d
Suslova Karina <educationkarina08@gmail.com>
2026-09-25 23:34:03 +0300

    fix problems with circular import

--- src/clients/__init__.py
+++ src/clients/__init__.py
@@ -1,3 +1,3 @@
+from .create_s3_client import s3_client
 from .bucket_functions import ensure_bucket_exists
-from .create_s3_client import s3_client, BUCKET_NAME
 from .parquet_functions import write_parquet, read_parquet
